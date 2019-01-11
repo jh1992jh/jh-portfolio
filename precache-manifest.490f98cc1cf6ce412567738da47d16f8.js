d85cf99c7aea50c4d5a0afd0819791c0186c965c
jh1992jh <janihanninen@hotmail.com>
2019-01-11 02:28:26 +0200

    Updates

---
+++ precache-manifest.490f98cc1cf6ce412567738da47d16f8.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff5838d2f1d1f8e48d80a6ec1d3f18d4",
+    "url": "/jh-portfolio/static/media/reactlandingpage-mobile1.ff5838d2.webp"
+  },
+  {
+    "revision": "1bd93a130b7563cbaf6e",
+    "url": "/jh-portfolio/static/css/main.ea3aa34c.chunk.css"
+  },
+  {
+    "revision": "1a0d40abb3bf69e96fd0",
+    "url": "/jh-portfolio/static/js/1.1a0d40ab.chunk.js"
+  },
+  {
+    "revision": "306e0b3fe919bce7a77a",
+    "url": "/jh-portfolio/static/js/runtime~main.306e0b3f.js"
+  },
+  {
+    "revision": "7be10f74597112b446532094e34c1851",
+    "url": "/jh-portfolio/static/media/socialfeed-desktop.7be10f74.webp"
+  },
+  {
+    "revision": "5b104a6c3fd362a30d07bfc13a42740a",
+    "url": "/jh-portfolio/static/media/socialfeed-mobile1.5b104a6c.webp"
+  },
+  {
+    "revision": "5cc241a2b8a1471968ce174b7ea6ccd9",
+    "url": "/jh-portfolio/static/media/socialfeed-mobile2.5cc241a2.webp"
+  },
+  {
+    "revision": "b3b26b43ba496ade51ab5ca50e696cce",
+    "url": "/jh-portfolio/static/media/filtermatic-desktop.b3b26b43.webp"
+  },
+  {
+    "revision": "929de7c49b9fa22613922d255ac7f669",
+    "url": "/jh-portfolio/static/media/filtermatic-mobile1.929de7c4.webp"
+  },
+  {
+    "revision": "3921ee290c82302f3065114189636e4f",
+    "url": "/jh-portfolio/static/media/filtermatic-mobile2.3921ee29.webp"
+  },
+  {
+    "revision": "cf60c5e2972100783c221b3ea909e624",
+    "url": "/jh-portfolio/static/media/pieceofcake-desktop.cf60c5e2.webp"
+  },
+  {
+    "revision": "4b93145ed2acbc6fb5d6937206df7a12",
+    "url": "/jh-portfolio/static/media/pieceofcake-mobile1.4b93145e.webp"
+  },
+  {
+    "revision": "9e20b4cb12d2e6e8f621610658df649a",
+    "url": "/jh-portfolio/static/media/pieceofcake-mobile2.9e20b4cb.webp"
+  },
+  {
+    "revision": "7ce6bf6e7e3d0948af86a9d6d6351d5d",
+    "url": "/jh-portfolio/static/media/reactlandinpage-desktop.7ce6bf6e.webp"
+  },
+  {
+    "revision": "1bd93a130b7563cbaf6e",
+    "url": "/jh-portfolio/static/js/main.1bd93a13.chunk.js"
+  },
+  {
+    "revision": "3b789164580d26b673faef11f0da8ace",
+    "url": "/jh-portfolio/static/media/reactlandingpage-mobile2.3b789164.webp"
+  },
+  {
+    "revision": "900bf3792b10d641328dd4b6b1ce5637",
+    "url": "/jh-portfolio/static/media/socialfeed-desktop-fp.900bf379.png"
+  },
+  {
+    "revision": "189766b51b13f9f3121ad0b7c24f6339",
+    "url": "/jh-portfolio/static/media/socialfeed-mobile1-fp.189766b5.png"
+  },
+  {
+    "revision": "a4ec0144e3a949c53f2616f33a0a76ea",
+    "url": "/jh-portfolio/static/media/socialfeed-mobile2-fp.a4ec0144.png"
+  },
+  {
+    "revision": "def031bf7a700fc819c8520b51fe09ed",
+    "url": "/jh-portfolio/static/media/pieceofcake-desktop-fp.def031bf.png"
+  },
+  {
+    "revision": "8d8e8402a6ebad478feade067e41b0c1",
+    "url": "/jh-portfolio/static/media/pieceofcake-mobile1-fp.8d8e8402.png"
+  },
+  {
+    "revision": "55cefe38ca0c6e6a39c072552b91ae3b",
+    "url": "/jh-portfolio/static/media/pieceofcake-mobile2-fp.55cefe38.png"
+  },
+  {
+    "revision": "6d843097bce13953069c3d9b4557cf58",
+    "url": "/jh-portfolio/static/media/filtermatic-desktop-fp.6d843097.png"
+  },
+  {
+    "revision": "894e53197c4e653528e968a35b45fcd4",
+    "url": "/jh-portfolio/static/media/filtermatic-mobile1-fp.894e5319.png"
+  },
+  {
+    "revision": "fcd9e29479c3c345b4712ce0e2f03030",
+    "url": "/jh-portfolio/static/media/filtermatic-mobile2-fp.fcd9e294.png"
+  },
+  {
+    "revision": "56290ca6545306ff06ba6366d6483bdf",
+    "url": "/jh-portfolio/static/media/reactlandingpage-desktop-fp.56290ca6.png"
+  },
+  {
+    "revision": "e8f26aabea1d6e5bc3058a566580d400",
+    "url": "/jh-portfolio/static/media/reactlandingpage-mobile1-fp.e8f26aab.png"
+  },
+  {
+    "revision": "98d7bbad403dde21fcc51f6c6cfadc0b",
+    "url": "/jh-portfolio/static/media/reactlandingpage-mobile2-fp.98d7bbad.png"
+  },
+  {
+    "revision": "e29460accb7754e3bcc96f9b575c4029",
+    "url": "/jh-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jh-portfolio/precache-manifest.544484074e5d1fb8ea3c6a25e8a0e435.js"
+  "/jh-portfolio/precache-manifest.490f98cc1cf6ce412567738da47d16f8.js"
 );
 
 workbox.clientsClaim();
